d802f2b070ea8a6d8eb47564f93a607500d55023
Toby Mackenzie <toby@cogneato.com>
2011-04-22 17:26:15 -0400

    clientbehavior/eventhandlers/nav: reupped from isf

--- eventHandlers/navigationHandler.js
+++ eventHandlers/navigationHandler.js
@@ -4,6 +4,33 @@ handles clicks on a selection of elements
 -----parameters
 
 -----instantiation
+__.scrOnload = function(){
+	var videoNavGrid = document.getElementById(__.cfg.idVideoNavGrid);		
+	if(videoNavGrid){
+		__.mainnavigationHandler = new __.classes.navigationHandler({
+			elmsItems: videoNavGrid.getElementsByTagName("a")
+			,doPreventDefault: false
+			,oninit: function(){
+				this.boot.elmVideoPlayerYoutube.style.left = __.cfg.offscreen;
+			}
+			,onswitch: function(arguments){
+
+				var fncThis = this;
+				var elmThis = arguments.elmThis;
+				var elmID = elmThis.getAttribute(fncThis.boot.attrID);
+				if(elmThis.getAttribute(fncThis.boot.attrType) == "youtube"){
+					fncThis.boot.elmVideoPlayerLocal.style.left = __.cfg.offscreen;
+					fncThis.boot.elmVideoPlayerYoutube.style.left = "0";
+				}else{
+					fncThis.boot.elmVideoPlayerLocal.style.left = "0";
+					fncThis.boot.elmVideoPlayerYoutube.style.left = __.cfg.offscreen;
+ 				}
+				highlightVideo(__.cfg.idVideoNavGrid,'video'+elmID);			
+			}
+			,boot: {elmVideoBox: document.getElementById(__.cfg.idVideoBox), elmVideoPlayerLocal: document.getElementById(__.cfg.idVideoPlayerLocal), elmVideoPlayerYoutube: document.getElementById(__.cfg.idVideoPlayerYoutube), attrType: "data-type", attrID: "data-id"}
+		});
+	}
+}
 */
 
 /*-------
@@ -16,6 +43,7 @@ __.classes.navigationHandler = function(arguments){
 		this.boot = arguments.boot || null;
 		this.classCurrent = arguments.classCurrent || "current";
 		this.doPreventDefault = (typeof arguments.doPreventDefault != "undefined")? arguments.doPreventDefault: true;
+		this.oninit = arguments.oninit || null;
 		this.onpreswitch = arguments.onpreswitch || null;
 		this.onpreswitchtest = arguments.onpreswitchtest || null;
 		this.onpostswitch = arguments.onpostswitch || null;
@@ -23,20 +51,24 @@ __.classes.navigationHandler = function(arguments){
 
 		//--derived attributes
 		this.inprogress = false;
-		
+
 		//--attach events
 		this.attachEvents(this.elmsItems);
+		
+		//--init
+		if(this.oninit)
+			this.oninit.call(this);
 	}
 	__.classes.navigationHandler.prototype.attachEvents = function(arguments){
 		var fncThis = this;
 		
-		var fncCallback = function(event){
+		var fncCallback = function(event, arg2, arg3, arg4){
 //->return
 			if(fncThis.inprogress == true)
 				return false;
 
 			var fncEvent = event;
-			var localVariables = {elmThis: this};
+			var localVariables = {elmThis: this, event: event};
 //-*add in stuff to find old and new elements
 //->return
 //			if(localVariables.oldItem[0] == localVariables.newItem[0]) return false;
@@ -58,10 +90,10 @@ __.classes.navigationHandler = function(arguments){
 			if(fncThis.onpreswitch)
 				fncThis.onpreswitch.call(fncThis, localVariables);
 
-/*-*add set class stuff
-			localVariables.oldItem.removeClass(fncThis.classCurrent);
-			localVariables.newItem.addClass(fncThis.classCurrent);
-*/
+//-*add set class stuff
+//			localVariables.oldItem.removeClass(fncThis.classCurrent);
+//			localVariables.newItem.addClass(fncThis.classCurrent);
+
 			if(fncThis.onswitch)
 				fncThis.onswitch.call(fncThis, localVariables);
 
@@ -73,7 +105,7 @@ __.classes.navigationHandler = function(arguments){
 			return (fncThis.doPreventDefault)? false: true;
 		}		
 
-		__.addListeners(fncThis.elmsItems, "click", fncCallback);
+		__.addListeners(fncThis.elmsItems, "mouseup", fncCallback);
 		__.addListeners(fncThis.elmsItems, "touch", fncCallback);
 	}
 
