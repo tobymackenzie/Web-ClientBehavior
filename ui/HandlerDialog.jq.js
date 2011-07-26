d7ad2795e81ae4eeb47001de4d001ed21f109281
Toby Mackenzie <toby@tobys-mini.local>
2011-07-26 17:00:28 -0400

    clientbehavior/ui/handlerdialog: added ability to change width on new content add, made default action, will want to move this width-checking out into lib later 	added a handling of height to go up to a max height and then add scrollbars 	bugfix: wasn't using dialogArguments at all 	removed popupbox class from default dialogArguments 	whitespace

--- ui/HandlerDialog.jq.js
+++ ui/HandlerDialog.jq.js
@@ -12,59 +12,84 @@ jqueryui: dialog
 -----css
 */
 
-/*-------
-©classname
--------- */
 /*---------
 @HandlerDialog
 ----------*/
 __.classes.HandlerDialog = function(arguments){
-        if(typeof arguments == "undefined") var arguments = {};
+		if(typeof arguments == "undefined") var arguments = {};
 		//--required attributes
 //->return
 		//--optional attributes
-        this.ajaxData = $.extend({ajaxcall: 1}, (arguments.ajaxData || null));
-        this.boot = arguments.boot || {};
-        this.dialogArguments = {autoOpen: false, dialogClass: "popupbox"};
-        this.dialogArguments = $.extend(this.dialogArguments, (arguments.dialogArguments || {}));
-        this.oninit = arguments.oninit || null;
-        this.onshow = arguments.onshow || null;
-        this.onsuccess = arguments.onsuccess || null;
-        this.url = arguments.url || null;
+		this.ajaxData = $.extend({ajaxcall: 1}, (arguments.ajaxData || null));
+		this.boot = arguments.boot || {};
+		this.dialogArguments = {autoOpen: false};
+		this.dialogArguments = $.extend(this.dialogArguments, (arguments.dialogArguments || {}));
+		this.doManageWidth = (typeof arguments.doManageWidth != "undefined")? arguments.doManageWidth: true;
+		this.doManageHeight = (typeof arguments.doManageHeight != "undefined")? arguments.doManageHeight: true;
+		this.oninit = arguments.oninit || null;
+		this.onshow = arguments.onshow || null;
+		this.onsuccess = arguments.onsuccess || null;
+		this.widthAdded = arguments.widthAdded || 0;
+		this.widthScrollbar = (typeof arguments.widthScrollbar != "undefined")? arguments.widthScrollbar: 25;
+		this.heightMax = arguments.heightMax || 500;
+		this.url = arguments.url || null;
 
-        this.elmDialog = null;
+		this.elmDialog = null;
 		//--derived attributes
-        var fncThis = this;
+		var fncThis = this;
 
-        //--do something
-        if(this.oninit)
-            this.oninit.call(this);
-    }
-    __.classes.HandlerDialog.prototype.initDialog = function(){
-        if(!this.elmDialog){
-            this.elmDialog = $("<div>");
-            this.elmDialog.dialog({dialogClass: "popupbox", autoOpen: false});
-        }
-    }
-    __.classes.HandlerDialog.prototype.showWithContent = function(argContent){
-        this.initDialog();
-        this.elmDialog.html(argContent);
-        this.elmDialog.dialog("open");
-        if(this.onshow)
-            this.onshow.call(this);
-    }
-    __.classes.HandlerDialog.prototype.showForAjax = function(arguments){
-        var fncThis = this;
-        if(typeof arguments.url == "undefined")
-            arguments.url = this.url;
-        arguments.data = $.extend({}, this.ajaxData, (arguments.data || null));
-        if(typeof arguments.success == "undefined")
-            arguments.success = function(data){ fncThis.callbackAjaxSuccess(data, this) };
+		//--do something
+		if(this.oninit)
+			this.oninit.call(this);
+	}
+	__.classes.HandlerDialog.prototype.initDialog = function(){
+		if(!this.elmDialog){
+			this.elmDialog = $("<div>");
+			if(this.doManageHeight)
+				this.elmDialog.css({overflow: "auto"});
+			this.elmDialog.dialog(this.dialogArguments);
+		}
+	}
+	__.classes.HandlerDialog.prototype.showWithContent = function(argContent){
+		this.initDialog();
+		var elmNewHTML = $(argContent);
+		this.elmDialog.html(elmNewHTML);
+		if(this.doManageWidth || this.doManageHeight){
+			var elmNewHTMLClone = $("<div>").html(elmNewHTML.clone());
+			elmNewHTMLClone.css({position: "absolute", left: "-9000px", top: "-9000px", display: "table"});
+			$("body").append(elmNewHTMLClone);
+			var widthNewHTML = elmNewHTMLClone.width();
+			widthNewHTML += this.widthAdded;
+			var heightNewHTML = elmNewHTMLClone.height();
+			if(this.doManageHeight && heightNewHTML > this.heightMax){
+				widthNewHTML += this.widthScrollbar;
+			}
+			elmNewHTMLClone.remove();
+			if(this.doManageWidth)
+				this.elmDialog.dialog("option", "width", widthNewHTML);
+			if(this.doManageHeight){
+				if(heightNewHTML > this.heightMax){
+					this.elmDialog.dialog("option", "height", this.heightMax);
+				}else
+					this.elmDialog.dialog("option", "height", "auto");
+			}
+		}
+		this.elmDialog.dialog("open");
+		if(this.onshow)
+			this.onshow.call(this);
+	}
+	__.classes.HandlerDialog.prototype.showForAjax = function(arguments){
+		var fncThis = this;
+		if(typeof arguments.url == "undefined")
+			arguments.url = this.url;
+		arguments.data = $.extend({}, this.ajaxData, (arguments.data || null));
+		if(typeof arguments.success == "undefined")
+			arguments.success = function(data){ fncThis.callbackAjaxSuccess(data, this) };
 
-        $.ajax(arguments);
-    }
-    __.classes.HandlerDialog.prototype.callbackAjaxSuccess = function(argData, argContext){
-        if(this.onsuccess)
-            argData = this.onsuccess.call(this, argData) || argData;
-        this.showWithContent(argData);
-    }
+		$.ajax(arguments);
+	}
+	__.classes.HandlerDialog.prototype.callbackAjaxSuccess = function(argData, argContext){
+		if(this.onsuccess)
+			argData = this.onsuccess.call(this, argData) || argData;
+		this.showWithContent(argData);
+	}
