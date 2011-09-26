ab385418475594a3b99b521f944d222669ac76ec
Toby Mackenzie <tobym@tobymackenzie.com>
2011-09-26 17:03:37 -0400

    cb/ui/handlerdialog: minor fixes 	height management now done as a callback that can be overriden by configuration 	defaultManageWidthHeight local variables now done in lcl 	changed name of width/heightAdded to width/heightModifier with suffix indication which is being modified	put default "margin" around the viewport managed height 		BUG: viewport height now seems to be wrong, not sure why, can't figure out how to get actual height needed

--- ui/HandlerDialog.jq.js
+++ ui/HandlerDialog.jq.js
@@ -16,7 +16,7 @@ jqueryui: dialog
 @HandlerDialog
 ----------*/
 __.classes.HandlerDialog = function(args){
-		if(typeof args== "undefined") var args= {};
+		if(typeof args == "undefined") var args = {};
 		var fncThis = this;
 		//--required attributes
 //->return
@@ -24,6 +24,7 @@ __.classes.HandlerDialog = function(args){
 		this.ajaxData = jQuery.extend({ajaxcall: 1}, (args.ajaxData || null));
 		this.boot = args.boot || {};
 		this.classLoading = args.classLoading || "loading";
+		this.clbManageWidthHeight = args.clbManageWidthHeight || this.defaultManageWidthHeight;
 		this.dialogArguments = {
 			autoOpen: false
 			,close: function(event, ui){
@@ -36,13 +37,15 @@ __.classes.HandlerDialog = function(args){
 		this.dialogArguments = jQuery.extend(this.dialogArguments, (args.dialogArguments || {}));
 		this.doManageWidth = (typeof args.doManageWidth != "undefined")? args.doManageWidth: true;
 		this.doManageHeight = (typeof args.doManageHeight != "undefined")? args.doManageHeight: true;
-		this.htmlLoading = arguments.htmlLoading || "";
+		this.htmlLoading = args.htmlLoading || "";
 		this.oninit = args.oninit || null;
 		this.onshow = args.onshow || null;
 		this.onsuccess = args.onsuccess || null;
-		this.widthAdded = args.widthAdded || 0;
+		this.widthModifierContent = args.widthModifierContent || 0;
 		this.widthScrollbar = (typeof args.widthScrollbar != "undefined")? args.widthScrollbar: __.lib.getWidthScrollbar();
-		this.heightMax = args.heightMax || 500;
+		this.heightMax = args.heightMax || "viewport";
+		this.heightModifierContent = args.heightModifierContent || 0;
+		this.heightModifierContainer = (typeof args.heightModifierContainer != "undefined")? args.heightModifierContainer: -40;
 		this.url = args.url || null;
 
 		this.request = null;
@@ -79,37 +82,7 @@ __.classes.HandlerDialog = function(args){
 		var elmNewHTML = jQuery(argContent);
 		this.elmDialog.html(elmNewHTML);
 		if(this.doManageWidth || this.doManageHeight){
-			var elmNewHTMLClone = jQuery('<div class="ui-dialog">').html(elmNewHTML.clone());
-			elmNewHTMLClone.addClass("ui-dialog-content "+this.dialogArguments.dialogClass);
-			elmNewHTMLClone.css({position: "absolute", left: "-9000px", top: "-9000px", display: "table"});
-			jQuery("body").append(elmNewHTMLClone);
-			var widthNewHTML = elmNewHTMLClone.width();
-			widthNewHTML += this.widthAdded;
-			var heightNewHTML = elmNewHTMLClone.height();
-			elmNewHTMLClone.remove();
-
-			if(this.doManageHeight){
-				if(this.heightMax == "viewport"){
-					var lclHeightMax = $(window).height();
-				}else if(isNumeric(this.heightMax)){
-					var lclHeightMax = this.heightMax;
-				}else{
-					var lclHeightMax = false;
-				}
-				if(lclHeightMax && heightNewHTML > lclHeightMax){
-					this.isLimitingHeight = true;
-					this.elmDialog.dialog("option", "height", lclHeightMax);
-				}else{
-					this.isLimitingHeight = false;
-					this.dialog("option", "height", "auto");
-				}
-			}
-			if(this.doManageWidth){
-				if(this.isLimitingHeight){
-					widthNewHTML += this.widthScrollbar;
-				}
-				this.dialog("option", "width", widthNewHTML);
-			}
+			this.clbManageWidthHeight.call(this, elmNewHTML);
 		}
 		this.dialog("open");
 		if(this.onshow)
@@ -142,7 +115,7 @@ __.classes.HandlerDialog = function(args){
 	}
 	__.classes.HandlerDialog.prototype.callbackAjaxSuccess = function(argData, argContext){
 		if(this.onsuccess)
- 		   argData = this.onsuccess.call(this, argData) || argData;
+			argData = this.onsuccess.call(this, argData) || argData;
 		this.showWithContent(argData);
 		this.elmDialog.removeClass(this.classLoading);
 		this.dialog("option", "position", this.dialog("option", "position"));
@@ -151,7 +124,39 @@ __.classes.HandlerDialog = function(args){
 	__.classes.HandlerDialog.prototype.dialog = function(){
 		return this.elmDialog.dialog.apply(this.elmDialog, arguments);
 	}
-	__.classes.HandlerDialog.prototype.defaultClbGetLoadingHTML = function(){
-		return "";
+	__.classes.HandlerDialog.prototype.defaultManageWidthHeight = function(argElmNewHTML){
+		var lcl = {};
+		lcl.elmNewHTMLClone = jQuery('<div class="ui-dialog">').html(argElmNewHTML.clone());
+		lcl.elmNewHTMLClone.addClass("ui-dialog-content "+this.dialogArguments.dialogClass);
+		lcl.elmNewHTMLClone.css({position: "absolute", left: "-9000px", top: "-9000px", display: "table"});
+		jQuery("body").append(lcl.elmNewHTMLClone);
+		lcl.widthNewHTML = lcl.elmNewHTMLClone.width();
+		lcl.widthNewHTML += this.widthModifierContent;
+		lcl.heightNewHTML = lcl.elmNewHTMLClone.height();
+		lcl.heightNewHTML += this.heightModifierContent;
+		lcl.elmNewHTMLClone.remove();
+
+		if(this.doManageHeight){
+			if(this.heightMax == "viewport"){
+				lcl.heightMax = $(window).height() + this.heightModifierContainer;
+			}else if(isNumeric(this.heightMax)){
+				lcl.heightMax = this.heightMax;
+			}else{
+				lcl.heightMax = false;
+			}
+			if(lcl.heightMax && lcl.heightNewHTML > lcl.heightMax){
+				this.isLimitingHeight = true;
+				this.elmDialog.dialog("option", "height", lcl.heightMax);
+			}else{
+				this.isLimitingHeight = false;
+				this.dialog("option", "height", "auto");
+			}
+		}
+		if(this.doManageWidth){
+			if(this.isLimitingHeight){
+				lcl.widthNewHTML += this.widthScrollbar;
+			}
+			this.dialog("option", "width", lcl.widthNewHTML);
+		}
 	}
 
