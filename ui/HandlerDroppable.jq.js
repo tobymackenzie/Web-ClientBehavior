2af06242496069c973f38508813a714bbb47b2e3
Toby Mackenzie <tobym@tobymackenzie.com>
2011-11-14 14:19:19 -0500

    cb/ui/handlerdroppable: entab

--- ui/HandlerDroppable.jq.js
+++ ui/HandlerDroppable.jq.js
@@ -15,57 +15,57 @@ jquery ui: droppable
 ©HandlerDroppable
 -------- */
 __.classes.HandlerDroppable = function(args){
-        this.element = args.element || null;
-        this.arrOptionsEvent = args.arrOptionsEvent || [
-            "activate"
-            ,"create"
-            ,"deactivate"
-            ,"drop"
-            ,"over"
-            ,"out"
-        ];
-        this.addd(args);        
-    }
-    __.classes.HandlerDroppable.prototype.addd = function(args){
-        var lcl = {};
-        if(!args.element)
-            args.element = this.element;
-        //--if already a droppable, add applicable old settings to new
-        if(args.element.hasClass("ui-droppable")){
-            //-#must punch first to keep accept intact
-            for(var i=0; i<this.arrOptionsEvent.length; ++i){            
-                this.punchOption(this.arrOptionsEvent[i], args);
-            }
-            this.concatenateOption("accept", args);
-            this.concatenateOption("activeClass", args, " ");
-            this.concatenateOption("hoverClass", args, " ");
-        }
-        args.element.droppable(args);
-    }
-    __.classes.HandlerDroppable.prototype.concatenateOption = function(argName, argsObject, argJoinWith){
-        if(typeof argJoinWith == "undefined") var argJoinWith = ",";
-        var lclOption = argsObject.element.droppable("option", argName);
-        if(lclOption){
-            if(argsObject[argName])
-                argsObject[argName] += argJoinWith;
-            argsObject[argName] += lclOption;
-        }
-    }
-    __.classes.HandlerDroppable.prototype.punchOption = function(argName, argsObject){
-        var lclOldOption = argsObject.element.droppable("option", argName);
-        if(lclOldOption){
-            if(argsObject[argName]){
-                var lclNewOption = argsObject[argName];
-                var lclSelector = argsObject["accept"];
-                argsObject[argName] = function(argEvent, argUI){
-                    if(argUI.draggable.is(lclSelector))
-                        return lclNewOption.apply(this, arguments);
-                    else
-                        return lclOldOption.apply(this, arguments);
-                }
-            }else{
-                argsObject[argName] = lclOldOption;
-            }
-        }
-    }
+		this.element = args.element || null;
+		this.arrOptionsEvent = args.arrOptionsEvent || [
+			"activate"
+			,"create"
+			,"deactivate"
+			,"drop"
+			,"over"
+			,"out"
+		];
+		this.addd(args);		
+	}
+	__.classes.HandlerDroppable.prototype.addd = function(args){
+		var lcl = {};
+		if(!args.element)
+			args.element = this.element;
+		//--if already a droppable, add applicable old settings to new
+		if(args.element.hasClass("ui-droppable")){
+			//-#must punch first to keep accept intact
+			for(var i=0; i<this.arrOptionsEvent.length; ++i){			 
+				this.punchOption(this.arrOptionsEvent[i], args);
+			}
+			this.concatenateOption("accept", args);
+			this.concatenateOption("activeClass", args, " ");
+			this.concatenateOption("hoverClass", args, " ");
+		}
+		args.element.droppable(args);
+	}
+	__.classes.HandlerDroppable.prototype.concatenateOption = function(argName, argsObject, argJoinWith){
+		if(typeof argJoinWith == "undefined") var argJoinWith = ",";
+		var lclOption = argsObject.element.droppable("option", argName);
+		if(lclOption){
+			if(argsObject[argName])
+				argsObject[argName] += argJoinWith;
+			argsObject[argName] += lclOption;
+		}
+	}
+	__.classes.HandlerDroppable.prototype.punchOption = function(argName, argsObject){
+		var lclOldOption = argsObject.element.droppable("option", argName);
+		if(lclOldOption){
+			if(argsObject[argName]){
+				var lclNewOption = argsObject[argName];
+				var lclSelector = argsObject["accept"];
+				argsObject[argName] = function(argEvent, argUI){
+					if(argUI.draggable.is(lclSelector))
+						return lclNewOption.apply(this, arguments);
+					else
+						return lclOldOption.apply(this, arguments);
+				}
+			}else{
+				argsObject[argName] = lclOldOption;
+			}
+		}
+	}
 
