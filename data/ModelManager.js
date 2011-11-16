821bb68fd90b83b4454945b0dfca1b8c869313fe
Toby Mackenzie <tobym@tobymackenzie.com>
2011-11-16 13:54:31 -0500

    cb/data: init folder for storing data related stuff, moved collection in here cb/data/modelmanager: init from partsgopher

---
+++ data/ModelManager.js
@@ -0,0 +1,83 @@
+/*
+simple model handling that can fetch from and persist to server data mode, not much else currently, simply passed callback to get model data
+
+-----dependencies
+tmlib: merge
+jquery
+
+-----parameters
+-----instantiation
+-----html
+-----css
+*/
+
+/*-------
+©ModelManager
+-------- */
+__.classes.ModelManager = function(args){
+		if(typeof args == "undefined") args = {};
+		//--optional attributes
+		this.boot = args.boot || {};
+		this.clbFetch = args.clbFetch || null;
+		this.clbPersist = args.clbPersist || null;
+		this.fnGetRequestData = args.fnGetRequestData || this.defaultFnGetRequestData;
+		this.oninit = args.oninit || null;
+		this.optionsFetch = args.optionsFetch || {};
+		this.optionsPersist = args.optionsPersist || {};
+		this.urlFetch = args.urlFetch || null;
+		this.urlPersist = args.urlPersist || null;
+	
+		//--derived attributes
+		this.binder = $({});
+
+		if(this.oninit) this.oninit.call(this);
+	}
+	__.classes.ModelManager.prototype.bind = function(){
+		this.binder.bind.apply(this.binder, arguments);
+	}
+	__.classes.ModelManager.prototype.data = function(){
+		this.binder.data.apply(this.binder, arguments);
+	}
+	__.classes.ModelManager.prototype.store = function(){
+		var data = this.fnGetRequestData.apply(this, arguments);
+		this.persist(data, this.urlPersist, this.clbPersist);
+	}
+	__.classes.ModelManager.prototype.defaultFnGetRequestData = function(args){
+		var fncReturn = [];
+		if(typeof args != "undefined"){
+			if(__.isArray(args))
+				fncReturn = args;
+			else
+				fncReturn = [args];
+		}
+		return fncReturn;
+	}
+	__.classes.ModelManager.prototype.fetch = function(argData, argURL, argCallback){
+		var lclThis = this;
+		var lclCallback = (typeof argCallback != "undefined")? argCallback: this.clbFetch;
+		var lclParams = {
+			success: function(){
+				if(lclCallback)
+					lclCallback.apply(lclThis, arguments);
+			}
+		};
+		if(argData)
+			lclParams.data = JSON.stringify(argData);
+		lclParams.url = argURL || this.urlFetch;
+		jQuery.ajax(__.lib.merge(this.optionsFetch, lclParams));
+	}
+	__.classes.ModelManager.prototype.persist = function(argData, argURL, argCallback){
+		var lclThis = this;
+		var lclCallback = (typeof argCallback != "undefined")? argCallback: this.clbPersist;
+		var lclParams = {
+			success: function(){
+				if(lclCallback)
+					lclCallback.apply(lclThis, arguments);
+			}
+		};
+		if(argData)
+			lclParams.data = JSON.stringify(argData);
+		lclParams.url = argURL || this.urlPersist;
+		jQuery.ajax(__.lib.merge(this.optionsPersist, lclParams));
+	}
+
