d1d4e2ed2d0f4de6d2940bf834d647a131a26b85
Toby Mackenzie <tobym@tobymackenzie.com>
2012-10-01 11:40:57 -0400

    menus/horizontal: added 'onOpen' and 'onClose' callbacks 	camelcase classes

--- menus/horizontal.js
+++ menus/horizontal.js
@@ -4,17 +4,17 @@ tmlib: addListeners, getElementsByClassName, addClass, removeClass, hasClass, is
 
 -----styling
 /*--base
-#topnavigation .submenu{
+#topnavigation .subMenu{
 	display: none;
 }
-#topnavigation .topitem.selected .submenu, #topnavigation .topitem:hover .submenu, #topnavigation .topitem:focus .submenu{
+#topnavigation .topItem.selected .subMenu, #topnavigation .topItem:hover .subMenu, #topnavigation .topItem:focus .subMenu{
 	display: block;
 }
 
 -----instantiation
 __.cfg.navigationID = 'mainnavigation';
 __.cfg.menuPadding = 20;
-__.cfg.submenuOffset = 0;
+__.cfg.subMenuOffset = 0;
 __.cfg.addedWidth = 20;
 
 
@@ -23,11 +23,11 @@ __.scrOnload = function(){
 	__.topnavigationDropdownhandler = new __.classes.suckerfish({menuID: __.cfg.navigationID});
 
 // size and center 1
-//	__.topnavigationDropdownhandler = new __.classes.suckerfish({menuID: __.cfg.navigationID, menuPadding: __.cfg.submenuPadding,submenuOffset: __.cfg.submenuOffset, addedWidth: __.cfg.addedWidth});
+//	__.topnavigationDropdownhandler = new __.classes.suckerfish({menuID: __.cfg.navigationID, menuPadding: __.cfg.subMenuPadding,subMenuOffset: __.cfg.subMenuOffset, addedWidth: __.cfg.addedWidth});
 //	oTopnavigationDropdownhandler.sizeAndCenter1();
 
 // size and center 2
-//	__.topnavigationDropdownhandler = new __.classes.suckerfish({menuID: __.cfg.navigationID, menuPadding: __.cfg.menuPadding, submenuOffset: __.cfg.submenuOffset, addedWidth: __.cfg.addedWidth});
+//	__.topnavigationDropdownhandler = new __.classes.suckerfish({menuID: __.cfg.navigationID, menuPadding: __.cfg.menuPadding, subMenuOffset: __.cfg.subMenuOffset, addedWidth: __.cfg.addedWidth});
 //	__.topnavigationDropdownhandler.sizeAndCenter2();
 }
 
@@ -35,15 +35,17 @@ __.scrOnload = function(){
 ©TMlib suckerfish
 ----------*/
 __.classes.suckerfish = function (args){
-		this.wrapperClass = (args.wrapperClass)? args.wrapperClass : 'topitem';
-		this.toplevelClass = (args.toplevelClass)? args.toplevelClass : 'toplevel';
-		this.submenuClass = (args.submenuClass)? args.submenuClass : 'submenu';
+		this.wrapperClass = (args.wrapperClass)? args.wrapperClass : 'topItem';
+		this.topLevelClass = (args.topLevelClass)? args.topLevelClass : 'topLevel';
+		this.subMenuClass = (args.subMenuClass)? args.subMenuClass : 'subMenu';
 		this.doCloseOnClickOutside = args.doCloseOnClickOutside || false;
 		this.doSizeAndCenter = (args.doSizeAndCenter)? args.doSizeAndCenter : false;
 		this.doSizeAndCenterFirst = (typeof args.doSizeAndCenterFirst != 'undefined')? args.doSizeAndCenterFirst : true;
 		this.doSizeAndCenterLast = (typeof args.doSizeAndCenterLast != 'undefined')? args.doSizeAndCenterLast : true;
 		this.menuPadding = (args.menuPadding)? args.menuPadding : 0;
-		this.submenuOffset = (args.submenuOffset)? args.submenuOffset : 0;
+		this.onClose = args.onClose || null;
+		this.onOpen = args.onOpen || null;
+		this.subMenuOffset = (args.subMenuOffset)? args.subMenuOffset : 0;
 		this.addedWidth = (args.addedWidth)? args.addedWidth : 0;
 		this.classSelected = args.classSelected || 'selected';
 
@@ -60,9 +62,9 @@ __.classes.suckerfish = function (args){
 		var fncReturnArray = new Array();
 		for(var i=0; i < this.elmsWrapper.length; ++i){
 			var forArray = new Array;
-			var forElmToplevel = __.lib.getElementsByClassName({'className': this.toplevelClass, 'element': this.elmsWrapper[i]});
-			var forElmSubmenu = __.lib.getElementsByClassName({'className': this.submenuClass, 'element': this.elmsWrapper[i]});
-			fncReturnArray.push({'elmItemWrapper':this.elmsWrapper[i], 'elmToplevel':forElmToplevel[0], 'elmSubmenu': (forElmSubmenu[0])?forElmSubmenu[0]:null})
+			var forElmtopLevel = __.lib.getElementsByClassName({'className': this.topLevelClass, 'element': this.elmsWrapper[i]});
+			var forelmSubMenu = __.lib.getElementsByClassName({'className': this.subMenuClass, 'element': this.elmsWrapper[i]});
+			fncReturnArray.push({'elmItemWrapper':this.elmsWrapper[i], 'elmtopLevel':forElmtopLevel[0], 'elmSubMenu': (forelmSubMenu[0])?forelmSubMenu[0]:null})
 		}
 
 		return fncReturnArray;
@@ -71,7 +73,7 @@ __.classes.suckerfish = function (args){
 		var fncThis = this;
 		for(var i=0;i < this.elmsMenuAssociations.length; ++i){
 			var forElmMenuItemArray = this.elmsMenuAssociations[i];
-			if(this.elmsMenuAssociations[i]['elmSubmenu']){
+			if(this.elmsMenuAssociations[i]['elmSubMenu']){
 				var callbackFull = function(forElmMenuItemArray, fncThis) {
 					return function(){
 						clearTimeout(fncThis.timeout);
@@ -86,15 +88,15 @@ __.classes.suckerfish = function (args){
 
 				}(fncThis);
 				__.lib.addListeners(forElmMenuItemArray['elmItemWrapper'], 'mouseover', callbackFull, false);
-				__.lib.addListeners(forElmMenuItemArray['elmToplevel'], 'mouseover', callbackFull, false);
-				__.lib.addListeners(forElmMenuItemArray['elmToplevel'], 'focus', callbackFull, false);
-				__.lib.addListeners(forElmMenuItemArray['elmSubmenu'], 'mouseover', callbackFull, false);
+				__.lib.addListeners(forElmMenuItemArray['elmtopLevel'], 'mouseover', callbackFull, false);
+				__.lib.addListeners(forElmMenuItemArray['elmtopLevel'], 'focus', callbackFull, false);
+				__.lib.addListeners(forElmMenuItemArray['elmSubMenu'], 'mouseover', callbackFull, false);
 				__.lib.addListeners(forElmMenuItemArray['elmItemWrapper'], 'click', callbackFull, false);
 				__.lib.addListeners(forElmMenuItemArray['elmItemWrapper'], 'touchstart', callbackFull, false);
 				__.lib.addListeners(forElmMenuItemArray['elmItemWrapper'], 'mouseout', callbackMouseout, false);
 
-				forElmMenuItemArray['elmToplevel'].href='javascript:/*openMenu()*/;';
-				forElmMenuItemArray['elmToplevel'].style.cursor = 'default';
+				forElmMenuItemArray['elmtopLevel'].href='javascript:/*openMenu()*/;';
+				forElmMenuItemArray['elmtopLevel'].style.cursor = 'default';
 			}
 			// empty menu items
 			else{
@@ -104,7 +106,7 @@ __.classes.suckerfish = function (args){
 					}
 				}(fncThis)
 				__.lib.addListeners(forElmMenuItemArray['elmItemWrapper'], 'mouseover', callbackEmpty, false);
-				__.lib.addListeners(forElmMenuItemArray['elmToplevel'], 'focus', callbackEmpty, false);
+				__.lib.addListeners(forElmMenuItemArray['elmtopLevel'], 'focus', callbackEmpty, false);
 				__.lib.addListeners(forElmMenuItemArray['elmItemWrapper'], 'click', callbackEmpty, false);
 				__.lib.addListeners(forElmMenuItemArray['elmItemWrapper'], 'touchstart', callbackEmpty, false);
 			}
@@ -114,17 +116,26 @@ __.classes.suckerfish = function (args){
 		}
 	}
 	__.classes.suckerfish.prototype.dropdownOpen = function(argElement){
-		if(this.elmCurrentlySelected && this.elmCurrentlySelected !== argElement)
+		var doOnOpen = (this.elmCurrentlySelected !== argElement)
+		if(this.elmCurrentlySelected && this.elmCurrentlySelected !== argElement){
 			this.dropdownCloseCurrent();
+		}
 		if(argElement){
 			__.lib.addClass(argElement, this.classSelected);
 			this.elmCurrentlySelected = argElement;
+			if(doOnOpen && this.onOpen){
+				this.onOpen.call(this, argElement);
+			}
 		}
 	}
 	__.classes.suckerfish.prototype.dropdownCloseCurrent = function(){
 		if(this.elmCurrentlySelected){
 			__.lib.removeClass(this.elmCurrentlySelected, this.classSelected);
+			var elmWasCurrent = this.elmCurrentlySelected;
 			this.elmCurrentlySelected = 0;
+			if(this.onClose){
+				this.onClose.call(this, elmWasCurrent);
+			}
 			return 1;
 		}
 		return 0;
@@ -132,6 +143,13 @@ __.classes.suckerfish = function (args){
 	__.classes.suckerfish.prototype.dropdownCloseAll = function(){
 		for(j=0;j < this.elmsMenuAssociations.length; ++j){
 			__.lib.removeClass(this.elmsMenuAssociations[j]['elmItemWrapper'], this.classSelected);
+		}
+		if(this.elmCurrentlySelected){
+			var elmWasCurrent = this.elmCurrentlySelected;
+			this.elmCurrentlySelected = 0;
+			if(this.onClose){
+				this.onClose.call(this, elmWasCurrent);
+			}
 		}
 	}
 
@@ -141,14 +159,14 @@ __.classes.suckerfish = function (args){
 	__.classes.suckerfish.prototype.sizeAndCenter1 = function(){
 		for(var i=0; i < this.elmsMenuAssociations.length; ++i){
 			var forElmMenuItemArray = this.elmsMenuAssociations[i];
-			if(forElmMenuItemArray['elmSubmenu'] && (this.doSizeAndCenterFirst || (i != 0)) && (this.doSizeAndCenterLast || (i != this.elmsMenuAssociations.length - 1))){
-				if(forElmMenuItemArray['elmSubmenu'].offsetWidth < forElmMenuItemArray['elmToplevel'].offsetWidth + this.addedWidth && !__.ua.isIE6()){
-					forElmMenuItemArray['elmSubmenu'].style.width = (forElmMenuItemArray['elmToplevel'].offsetWidth + this.addedWidth/*  - this.submenuPadding */) + 'px';
+			if(forElmMenuItemArray['elmSubMenu'] && (this.doSizeAndCenterFirst || (i != 0)) && (this.doSizeAndCenterLast || (i != this.elmsMenuAssociations.length - 1))){
+				if(forElmMenuItemArray['elmSubMenu'].offsetWidth < forElmMenuItemArray['elmtopLevel'].offsetWidth + this.addedWidth && !__.ua.isIE6()){
+					forElmMenuItemArray['elmSubMenu'].style.width = (forElmMenuItemArray['elmtopLevel'].offsetWidth + this.addedWidth/*  - this.subMenuPadding */) + 'px';
 				}
-				if(forElmMenuItemArray['elmSubmenu'].offsetWidth > forElmMenuItemArray['elmToplevel'].offsetWidth){
-					ifOffset = ((forElmMenuItemArray['elmToplevel'].offsetWidth - (forElmMenuItemArray['elmSubmenu'].offsetWidth/*  + this.ubmenuPadding */)) / 2);
-					if(__.ua.isIE6()) ifOffset += this.submenuOffset;
-					forElmMenuItemArray['elmSubmenu'].style.left = ifOffset+ 'px';
+				if(forElmMenuItemArray['elmSubMenu'].offsetWidth > forElmMenuItemArray['elmtopLevel'].offsetWidth){
+					ifOffset = ((forElmMenuItemArray['elmtopLevel'].offsetWidth - (forElmMenuItemArray['elmSubMenu'].offsetWidth/*  + this.submenuPadding */)) / 2);
+					if(__.ua.isIE6()) ifOffset += this.subMenuOffset;
+					forElmMenuItemArray['elmSubMenu'].style.left = ifOffset+ 'px';
 				}
 			}
 		}
@@ -156,19 +174,19 @@ __.classes.suckerfish = function (args){
 	__.classes.suckerfish.prototype.sizeAndCenter2 = function(){
 		for(var i=0; i < this.elmsMenuAssociations.length; ++i){
 			var forElmMenuItemArray = this.elmsMenuAssociations[i];
-			if(forElmMenuItemArray['elmSubmenu'] && (this.doSizeAndCenterFirst || (i != 0)) && (this.doSizeAndCenterLast || (i != this.elmsMenuAssociations.length - 1))){
-				forElmMenuItemArray['elmSubmenu'].style.visibility = 'hidden';
-				forElmMenuItemArray['elmSubmenu'].style.display = 'block';
-				if(forElmMenuItemArray['elmSubmenu'].offsetWidth < forElmMenuItemArray['elmToplevel'].offsetWidth + this.addedWidth && !__.ua.isIE6()){
-					forElmMenuItemArray['elmSubmenu'].style.width = (forElmMenuItemArray['elmToplevel'].offsetWidth + this.addedWidth + this.menuPadding*2) + 'px';
+			if(forElmMenuItemArray['elmSubMenu'] && (this.doSizeAndCenterFirst || (i != 0)) && (this.doSizeAndCenterLast || (i != this.elmsMenuAssociations.length - 1))){
+				forElmMenuItemArray['elmSubMenu'].style.visibility = 'hidden';
+				forElmMenuItemArray['elmSubMenu'].style.display = 'block';
+				if(forElmMenuItemArray['elmSubMenu'].offsetWidth < forElmMenuItemArray['elmtopLevel'].offsetWidth + this.addedWidth && !__.ua.isIE6()){
+					forElmMenuItemArray['elmSubMenu'].style.width = (forElmMenuItemArray['elmtopLevel'].offsetWidth + this.addedWidth + this.menuPadding*2) + 'px';
 				}
-				if(forElmMenuItemArray['elmSubmenu'].offsetWidth > forElmMenuItemArray['elmToplevel'].offsetWidth){
-					ifOffset = ((forElmMenuItemArray['elmToplevel'].offsetWidth - forElmMenuItemArray['elmSubmenu'].offsetWidth ) / 2) + this.menuPadding;
-					if(__.ua.isIE6()) ifOffset += this.submenuOffset;
-					forElmMenuItemArray['elmSubmenu'].style.left = ifOffset+ 'px';
+				if(forElmMenuItemArray['elmSubMenu'].offsetWidth > forElmMenuItemArray['elmtopLevel'].offsetWidth){
+					ifOffset = ((forElmMenuItemArray['elmtopLevel'].offsetWidth - forElmMenuItemArray['elmSubMenu'].offsetWidth ) / 2) + this.menuPadding;
+					if(__.ua.isIE6()) ifOffset += this.subMenuOffset;
+					forElmMenuItemArray['elmSubMenu'].style.left = ifOffset+ 'px';
 				}
-				forElmMenuItemArray['elmSubmenu'].style.display = '';
-				forElmMenuItemArray['elmSubmenu'].style.visibility = '';
+				forElmMenuItemArray['elmSubMenu'].style.display = '';
+				forElmMenuItemArray['elmSubMenu'].style.visibility = '';
 			}
 		}
 	}
