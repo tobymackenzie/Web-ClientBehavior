01fda972022f40bb24e6f302fe04fe15e909fd39
Toby Mackenzie <tobym@tmacbook.neo.rr.com>
2012-11-29 20:25:52 -0500

    core/Functions: ++contains()

--- core/Functions.js
+++ core/Functions.js
@@ -1,5 +1,25 @@
 __.core.Functions = {
 	/*
+	Function: contains
+	In support browsers, determines if a function contains a given string / matches a given regex.  Useful to see if a certain variable is used or function is called, particularly for checking if a child class calls a parent class's method.  For unsupported browsers, always returns true to allow them to function (may change this behavior in the future.)
+
+	Parameters:
+		argFunction(Function): the function to examine
+		argContains(RegExp|String): the string content to search the function for
+	*/
+	contains:
+		//--make sure functions can be tested
+		(/xyz/.test(function(){xyz;}))
+		? function(argFunction, argContains){
+			if(!(argContains instanceof RegExp)){
+				argContains = new RegExp(argContains, 'i');
+			}
+			return argContains.test(argFunction);
+		}
+		: function(){ return true; }
+
+	/*
+	Function: duckPunch
 	Duck punch a function with a wrapper, ie create a function that runs the wrapper with the original funciton available inside.  Unshifts the original function into the first parameter for the wrapper, since we cannot close into an already existing function.  Wrapper must shift off first argument as original function to be able to apply to said function.
 
 	Example:
@@ -21,7 +41,7 @@ __.core.Functions = {
 		newFunction('one', 'two');
 		(end code)
 	*/
-	'duckPunch': function(argFunction, argWrapper){
+	,'duckPunch': function(argFunction, argWrapper){
 	    return function(){
 	        Array.prototype.unshift.call(arguments, argFunction)
 	        argWrapper.apply(this, arguments);
