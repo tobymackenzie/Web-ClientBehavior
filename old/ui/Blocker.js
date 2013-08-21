cf321105232899176ba0b745770c1bdb4b129e2f
Toby Mackenzie <public@tobymackenzie.com>
2013-08-21 01:10:11 -0400

    ui/blocker: mergeInto() for properties.  linting.  class names

--- old/ui/Blocker.js
+++ old/ui/Blocker.js
@@ -1,35 +1,44 @@
+/* global __, jQuery */
 	/*-----
 	==Blocker
 	-----*/
 	__.classes.Blocker = function(argOptions){
-			if(typeof argOptions == 'undefined') argOptions = {};
-			this.elements = argOptions.elements || '<div class="Blocker">';
-			this.elmMessage = argOptions.elmMessage || jQuery('<div class="BlockerMessage">');
+			if(typeof argOptions == 'undefined'){
+				argOptions = {};
+			}
+			this.elements = argOptions.elements || '<div class="blocker">';
+			this.elmMessage = argOptions.elmMessage || jQuery('<div class="blockerMessage">');
 
 			this.hasMessageClass = argOptions.hasMessageClass || 'hasMessage';
 			this.noMessageClass = argOptions.noMessageClass || 'noMessage';
 
 			this.init(argOptions); //--'parent' constructor
-		}
-		__.core.Objects.mergeInto(__.classes.Blocker.prototype, __.Widget);
-		//--hide elements on add to DOM
-		__.classes.Blocker.prototype.onAddElementsToDOM = function(argElements){
-			argElements.hide();
-		}
-		__.classes.Blocker.prototype.show = function(argMessage){
-			if(typeof argMessage != 'undefined'){
-				this.elmMessage.html(argMessage);
-				this.elements
-					.append(this.elmMessage)
-					.addClass(this.hasMessageClass)
-					.removeClass(this.noMessageClass)
-				;
-			}else{
-				this.elmMessage.detach();
-				this.elements
-					.removeClass(this.hasMessageClass)
-					.addClass(this.noMessageClass)
-				;
+		};
+		__.core.Objects.mergeInto(
+			__.classes.Blocker.prototype
+			,__.mixins.Widget
+			,{
+				//--hide elements on add to DOM
+				onAddElementsToDOM: function(argElements){
+					argElements.hide();
+				}
+				,show: function(argMessage){
+					var _elements = this.getElements();
+					if(typeof argMessage != 'undefined'){
+						this.elmMessage.html(argMessage);
+						_elements
+							.append(this.elmMessage)
+							.addClass(this.hasMessageClass)
+							.removeClass(this.noMessageClass)
+						;
+					}else{
+						this.elmMessage.detach();
+						_elements
+							.removeClass(this.hasMessageClass)
+							.addClass(this.noMessageClass)
+						;
+					}
+					__.mixins.Widget.show.call(this); //--parent
+				}
 			}
-			__.widget.show.call(this); //--parent
-		}
+		);
