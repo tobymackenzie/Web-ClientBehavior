a2481b504a1f9f1ff380d166fca274cecdf8da79
Toby Mackenzie <public@tobymackenzie.com>
2013-08-22 22:32:09 -0400

    removed unused dependency wrap stuff

--- src/core/dependencyNonBaseInclude.js
+++
@@ -1,8 +0,0 @@
-//-# only set up tmlib if passed in, so base tmlib can instantiate itself and store original
-if(_dependencies.tmlib || false){
-	var __ = _dependencies.tmlib;
-	var tmlib = _dependencies.tmlib;
-}else if(_dependencies.__ || false){
-	var __ = _dependencies.__;
-	var tmlib = _dependencies.__;
-}
