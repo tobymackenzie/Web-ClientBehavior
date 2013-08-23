5c8f3e3a9790b1508f3c996589eaef1c445bb9c9
Toby Mackenzie <public@tobymackenzie.com>
2013-08-22 22:32:49 -0400

    test: split BaseClass tests out from classes tests

--- test/core/classes.js
+++ test/core/classes.js
@@ -336,47 +336,4 @@
 			);
 		});
 	}
-
-	/*=====
-	==Classes
-	=====*/
-	test('BaseClass', function(assert){
-		//==initial setup
-		var myInstance = new __.core.BaseClass({
-			instanceProperty1: 'value1'
-			,instanceProperty2: 'value2'
-			,instanceMethod1: function(){
-				return 'foo';
-			}
-		});
-		//==tests
-		//--test property initialization
-		assert.ok(typeof myInstance == 'object', 'myInstance should be an object');
-		assert.equal(myInstance.instanceProperty1, 'value1', 'myInstance should have a property "instanceProperty1" with value "value1');
-		assert.equal(myInstance.instanceProperty2, 'value2', 'myInstance should have a property "instanceProperty2" with value "value2');
-		assert.ok(typeof myInstance.instanceMethod1 == 'function', 'myInstance should have a property "instanceMethod1" that is a function');
-		assert.equal(myInstance.instanceMethod1(), 'foo', 'myInstance.instanceMethod1() should return the string "foo"');
-	});
-	test('BaseClass direct call', function(assert){
-		//==initial setup
-		var _instance = __.core.BaseClass({foo: 'foo', bar: 'bar'});
-		//==tests
-		assert.ok(
-			_instance instanceof __.core.BaseClass
-			,'created object should be instance of BaseClass'
-		);
-		assert.equal(_instance.foo, 'foo','arguments should be passed to init method');
-		assert.equal(_instance.bar, 'bar','arguments should be passed to init method');
-	});
-	test('BaseClass:create()', function(assert){
-		//==initial setup
-		var _instance = __.core.BaseClass.create({foo: 'foo', bar: 'bar'});
-		//==tests
-		assert.ok(
-			_instance instanceof __.core.BaseClass
-			,'created object should be instance of BaseClass'
-		);
-		assert.equal(_instance.foo, 'foo','arguments should be passed to init method');
-		assert.equal(_instance.bar, 'bar','arguments should be passed to init method');
-	});
 }));
