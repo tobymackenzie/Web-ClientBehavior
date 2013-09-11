02070b058c9fae458d8b0b3d5b513f660ef8c5f9
Toby Mackenzie <public@tobymackenzie.com>
2013-09-10 23:57:08 -0400

    fullContainerImage: use class system

--- old/fullContainerImage.jq.js
+++ old/fullContainerImage.jq.js
@@ -1,126 +1,136 @@
 /*
+Class: FullContainerImage
+
 makes image full width or height in container
 
------ css
-html, body{ // for full body
-	margin: 0;
-	padding: 0;
-	width: 100%;
-	height: 100%;
-}
-#pagewrap{ // for full body
-	min-width: 960px;
-	height: 100%;
-}
-#fullheighter{ // for full body
-	float: left;
-	position: relative;
-	z-index: -1;
-	width: 1px;
-	height: 100%;
-}
-#bgimage img{
-	position: fixed;
-	top: 0;
-	left: 0;
-}
-#bgimage img.fitwidth{
-	width: 100%;
-	height: auto;
-	min-height: 0;
-}
-#bgimage img.fitheight{
-	width: auto;
-	min-width: 0;
-	height: 100%;
-}
-html.uaielte6 #bgimage img{
-	position: absolute;
-}
+Styles:
+	html, body{ // for full body
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
+	}
+	#.pageWrap{ // for full body
+		min-width: 960px;
+		height: 100%;
+	}
+	#fullheighter{ // for full body
+		float: left;
+		position: relative;
+		z-index: -1;
+		width: 1px;
+		height: 100%;
+	}
+	.bgImage{
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
+	.bgImage.fitwidth{
+		width: 100%;
+		height: auto;
+		min-height: 0;
+	}
+	.bgImage.fitheight{
+		width: auto;
+		min-width: 0;
+		height: 100%;
+	}
+	html.ua-ie-lte6 .bgImage{
+		position: absolute;
+	}
 
------ html
-<body>
-<div id="pagewrap">
-	<div id="fullheighter"></div>
-	<div id="generalcontent"></div>
-</div>
-<div id="bgimage"><img src="" alt="" /></div>
-</body>
------ sources of information
--@http://css-tricks.com/perfect-full-page-background-image/
--@http://plugins.jquery.com/project/fullscreenr
+Markup:
+	<body>
+	<div id=".pageWrap">
+		<div id="fullheighter"></div>
+		<div id="generalcontent"></div>
+	</div>
+	<div class="bgImageWrap"><img class="bgImage" src="" alt="" /></div>
+	</body>
 
+Info:
+	-@http://css-tricks.com/perfect-full-page-background-image/
+	-@http://plugins.jquery.com/project/fullscreenr
 */
 /* global __, clearTimeout, jQuery, setTimeout, window */
-__.classes.fullContainerImage = function(args){
-		//--optional attributes
-		this.boot = args.boot || null;
-		this.elmContainer = args.elmContainer || jQuery(window);
-		this.classFitWidth = args.classFitWidth || 'fitwidth';
-		this.classFitHeight = args.classFitHeight || 'fitheight';
-		this.doCenterHorizontally = args.doCenterHorizontally || false;
-		this.doCenterVertically = args.doCenterVertically || false;
-		this.oninit = args.oninit || null;
+__.classes.FullContainerImage = __.core.Classes.create({
+	init: function(_args){
+		this.__base(arguments);
+		if(!this.$){
+			this.$ = jQuery(window);
+		}
 
 		//--derived attributes
-		var fncThis = this;
-		var fncElmImage = args.elmImage || null;
-		this.setImage(fncElmImage);
+		if(this.$image){
+			this.setImage(this.$image);
+		}
 
 		//--bind events
+		var _this = this;
 		var _timeout = null;
 		jQuery(window).on('resize', function(){
 			clearTimeout(_timeout);
 			_timeout = setTimeout(function(){
-				fncThis.setClass();
+				_this.setClass();
 			}, 200);
 		});
+
 		if(this.oninit){
 			this.oninit.call(this);
 		}
-	};
-	__.classes.fullContainerImage.prototype.setClass = function(){
-		if(this.elmImage.length > 0){
-			if((this.elmContainer.width() / this.elmContainer.height()) < this.aspectRatioImage ){
-				if(!this.elmImage.hasClass(this.classFitHeight)){
-					this.elmImage.removeClass(this.classFitWidth).addClass(this.classFitHeight);
-					if(this.doCenterVertically){
-						this.elmImage.css('top', 0);
+	}
+	,properties: {
+		$: undefined
+		,classFitWidth: 'fitwidth'
+		,classFitHeight: 'fitheight'
+		,doCenterHorizontally: false
+		,doCenterVertically: false
+		,oninit: null
+		,setClass: function(){
+			if(this.$image.length > 0){
+				if((this.$.width() / this.$.height()) < this.aspectRatioImage ){
+					if(!this.$image.hasClass(this.classFitHeight)){
+						this.$image.removeClass(this.classFitWidth).addClass(this.classFitHeight);
+						if(this.doCenterVertically){
+							this.$image.css('top', 0);
+						}
 					}
-				}
-				if(this.doCenterHorizontally){
-					this.elmImage.css('left', -((this.elmImage.width() - this.elmContainer.width()) / 2));
-				}
-			}else{
-				if(!this.elmImage.hasClass(this.classFitWidth)){
-					this.elmImage.removeClass(this.classFitHeight).addClass(this.classFitWidth);
 					if(this.doCenterHorizontally){
-						this.elmImage.css('left', 0);
+						this.$image.css('left', -((this.$image.width() - this.$.width()) / 2));
+					}
+				}else{
+					if(!this.$image.hasClass(this.classFitWidth)){
+						this.$image.removeClass(this.classFitHeight).addClass(this.classFitWidth);
+						if(this.doCenterHorizontally){
+							this.$image.css('left', 0);
+						}
+					}
+					if(this.doCenterVertically){
+						this.$image.css('top', -((this.$image.height() - this.$.height()) / 2));
 					}
 				}
-				if(this.doCenterVertically){
-					this.elmImage.css('top', -((this.elmImage.height() - this.elmContainer.height()) / 2));
+			}
+		}
+		,setImage: function($image){
+			var _this = this;
+			this.$image = $image;
+			if(this.$image.length > 0){
+				var _callback = function(){
+					var _widthHeight = __.lib.getHiddenElementWidthHeight(this.$image);
+					this.aspectRatioImage = _widthHeight.width / _widthHeight.height;
+					this.setClass();
+				};
+				if(this.$image.width() > 0){
+					_callback.call(this);
+				}else{
+					this.$image.on('load', function(){
+						_callback.call(_this);
+					});
 				}
-			}
-		}
-	};
-	__.classes.fullContainerImage.prototype.setImage = function(argElmImage){
-		var fncThis = this;
-		this.elmImage = argElmImage;
-		if(this.elmImage.length > 0){
-			var fncCallback = function(){
-				var fncWidthHeight = __.lib.getHiddenElementWidthHeight(this.elmImage);
-				this.aspectRatioImage = fncWidthHeight.width / fncWidthHeight.height;
-				this.setClass();
-			};
-			if(this.elmImage.width() > 0){
-				fncCallback.call(this);
 			}else{
-				this.elmImage.on('load', function(){
-					fncCallback.call(fncThis);
-				});
+				this.aspectRatio = false;
 			}
-		}else{
-			this.aspectRatio = false;
 		}
-	};
+	}
+});
