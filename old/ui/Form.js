7f787b850ec5f7a651129a5bd8f09732387974f7
Toby Mackenzie <public@tobymackenzie.com>
2013-10-03 23:30:39 -0400

    old/ui/Form: convert to class system.  allow setting of data to send with all requests.  update variable naming conventions

--- old/ui/Form.js
+++ old/ui/Form.js
@@ -1,71 +1,76 @@
-	/*-----
-	--Form
-	-----*/
-	__.classes.Form = function(argOptions){
-			this.inputSelector = argOptions.inputSelector || 'input:not([type=button],[type=image],[type=reset],[type=submit]),select,textarea';
-			this.onSend = argOptions.onSend || null;
-			this.onSuccess = argOptions.onSuccess || null;
-
-			if(typeof argOptions.onSubmit == 'function'){
-				this.onSubmit = argOptions.onSubmit;
-			}
-			if(typeof argOptions.action == 'undefined'){
-				this.actionType = 'deferred';
-				this.action = null;
-			}else{
-				this.actionType = 'fixed';
-				this.action = argOptions.action;
-			}
-			if(typeof argOptions.method == 'undefined'){
-				this.methodType = 'deferred';
-				this.method = 'post';
-			}else{
-				this.methodType = 'fixed';
-				this.method = argOptions.action;
-			}
-			this.init(argOptions); //--'parent' constructor
+/*
+Class: Form
+Form widget for submitting form via ajax
+*/
+/* global __, jQuery */
+__.classes.Form = __.core.Classes.create({
+	init: function(){
+		this.__base(arguments);
+		if(typeof this.action !== 'undefined'){
+			this.actionType = 'fixed';
 		}
-		__.core.Objects.mergeInto(__.classes.Form.prototype, __.Widget);
-		__.classes.Form.prototype.onSubmit = function(argValues, argEvent){
-			var lcThis = this;
-			if(lcThis.onSend){
-				lcThis.onSend.call(this, argValues, argEvent);
+		if(typeof this.method !== 'undefined'){
+			this.methodType = 'fixed';
+		}
+		__.mixins.Widget.properties.init.apply(this, arguments);
+	}
+	,mixins: [
+		__.mixins.Widget
+		,__.mixins.InitHandling
+	]
+	,properties: {
+		action: null
+		,actionType: 'deferred'
+		,data: null
+		,deinit: function(){
+			this.setElements(undefined);
+			__.mixins.InitHandling.properties.deinit.apply(this, arguments);
+		}
+		,inputSelector: 'input:not([type=button],[type=image],[type=reset],[type=submit]),select,textarea'
+		,method: 'post'
+		,methodType: 'deferred'
+		,onSend: null
+		,onSubmit: function(_values, _event){
+			var _this = this;
+			if(_this.onSend){
+				_this.onSend.call(this, _values, _event);
 			}
 			jQuery.ajax({
 				url: this.action
-				,data: argValues
+				,data: jQuery.extend(this.data, _values)
 				,type: this.method.toUpperCase()
-				,success: function(argResponse){
-					if(lcThis.onSuccess){
-						lcThis.onSuccess(argResponse);
+				,success: function(_response){
+					if(_this.onSuccess){
+						_this.onSuccess(_response);
 					}
 				}
 			});
 		}
-		__.classes.Form.prototype.reset = function(){
+		,onSuccess: null
+		,reset: function(){
 			this.elements.each(function(){
 				this.reset();
 			});
 		}
-		__.classes.Form.prototype.setElements = function(argElements){
+		,setElements: function(_elements){
 			//--unbind for old elements if a jQuery instance
 			if(this.elements && this.elements instanceof jQuery){
 				this.elements.off('submit', this.submitHandler);
 			}
 
 			//--set new elements
-			__.widget.setElements.call(this, argElements);
+			__.mixins.Widget.properties.setElements.call(this, _elements);
 
-			if(argElements && argElements instanceof jQuery){
+			if(_elements && _elements instanceof jQuery){
 				//--bind
 				this.elements.on('submit', jQuery.proxy(this.submitHandler, this));
 
 				//--set action if needed
 				if(this.actionType == 'deferred'){
 					//--set object action to action of form element
-					var action = this.elements.attr('action');
-					if(action){
-						this.action = action;
+					var _action = this.elements.attr('action');
+					if(_action){
+						this.action = _action;
 					//--if empty form action, attempt to use url that was passed in for form location or current href
 					}else{
 						this.action = this.elementsUrl;
@@ -75,22 +80,26 @@
 				//--set method if needed
 				if(this.methodType == 'deferred'){
 					//--set object method to method of form element, if it has one
-					var method = this.elements.attr('method');
-					if(method){
-						this.method = method;
+					var _method = this.elements.attr('method');
+					if(_method){
+						this.method = _method;
 					}
 				}
 			}
 		}
-		__.classes.Form.prototype.submitHandler = function(event){
-			var values = {};
+		,submitHandler: function(_event){
+			var _values = {};
 			this.elements.find(this.inputSelector).each(function(){
-				var elmThis = jQuery(this);
-				values[elmThis.attr('name')] = elmThis.val();
+				var $this = jQuery(this);
+				_values[$this.attr('name')] = $this.val();
 			});
 			if(this.onSubmit){
-				this.onSubmit.call(this, values, event);
+				this.onSubmit.call(this, _values, _event);
 			}
-			if(event.preventDefault) event.preventDefault();
+			if(_event.preventDefault){
+				_event.preventDefault();
+			}
 			return false;
 		}
+	}
+});
