8f86b2b8fba6aa5e269ae0946ec68688206cedd3
Toby Mackenzie <tobym@tobymackenzie.com>
2013-10-21 14:15:07 -0400

    old/ui/dialog: added close button.  fixed click handler to hide dialog

--- old/ui/Dialog.js
+++ old/ui/Dialog.js
@@ -22,18 +22,32 @@ __.classes.Dialog = __.core.Classes.create({
 	,mixins: __.mixins.Widget
 	,properties: {
 		$: '.dialog'
+		,$content: '.dialogContent'
+		,$closeButton: '<a class="dialogCloseButton closeButton button">Close Dialog</a>'
 		,addElmToDOM: function(_elm, _container){
 			_container.append(_elm);
 			return this;
 		}
 		,blocker: null
 		,container: undefined
-		,elements: '<div class="dialogWrapWrap"><div class="dialogWrap"><div class="dialog"></div></div></div>'
+		,elements: '<div class="dialogWrapWrap"><div class="dialogWrap"><div class="dialog"><div class="dialogContent"></div></div></div></div>'
 		,get$: function(){
 			if(typeof this.$ === 'string'){
 				this.loadElm(this.$, 'set$', this.getElements());
 			}
 			return this.$;
+		}
+		,get$Content: function(){
+			if(typeof this.$content === 'string'){
+				this.loadElm(this.$content, 'set$Content', this.get$());
+			}
+			return this.$content;
+		}
+		,get$CloseButton: function(){
+			if(typeof this.$closeButton === 'string'){
+				this.loadElm(this.$closeButton, 'set$CloseButton', this.get$());
+			}
+			return this.$closeButton;
 		}
 		,hide: function(){
 			__.mixins.Widget.properties.hide.apply(this, arguments);
@@ -43,7 +57,7 @@ __.classes.Dialog = __.core.Classes.create({
 			return this;
 		}
 		,loadElm: function(_elm, _setter, _container){
-			var _promise;
+			var _promise
 			var _this = this;
 			if(typeof _elm === 'string'){
 				var _newElm;
@@ -60,7 +74,7 @@ __.classes.Dialog = __.core.Classes.create({
 					_promise = _request.promise();
 				}else if(__.lib.isHTML(_elm)){
 					_newElm = jQuery(_elm);
-					this.addElmToDOM(_newElm);
+					this.addElmToDOM(_newElm, _container);
 					this[_setter](_newElm);
 				}else{
 					_newElm = _container.find(_elm);
@@ -78,22 +92,34 @@ __.classes.Dialog = __.core.Classes.create({
 			return _promise;
 		}
 		,set$: function(_$){
-			if(typeof this.$ === 'object'){
-				this.$.off('click', this.stopPropogationHandler);
-			}
 			this.$ = _$;
-			this.$.on('click', this.stopPropogationHandler);
+			if(this.get$CloseButton()){
+				this.get$CloseButton().on('click', jQuery.proxy(this.hide, this));
+			}
+			return this;
+		}
+		,set$Content: function(_$){
+			this.$content = _$;
+			return this;
+		}
+		,set$CloseButton: function(_$){
+			if(this.$closeButton instanceof jQuery){
+				this.get$CloseButton.off('click', jQuery.proxy(this.hide, this));
+			}
+			this.$closeButton = _$;
 			return this;
 		}
 		,setContent: function(_content){
-			this.get$().html(_content);
+			this.get$Content().html(_content);
 			return this;
 		}
 		,setElements: function(_elm){
 			var _this = this;
 			__.mixins.Widget.properties.setElements.call(this, _elm);
-			this.getElements().on('click', function(){
-				_this.hide();
+			this.getElements().on('click', '.dialogWrap', function(_event){
+				if(_event.target === this){
+					_this.hide();
+				}
 			});
 		}
 		,show: function(){
@@ -103,8 +129,6 @@ __.classes.Dialog = __.core.Classes.create({
 			}
 			return this;
 		}
-		,stopPropogationHandler: function(_event){
-			return _event.stopPropagation && _event.stopPropagation();
-		}
 	}
 });
+
