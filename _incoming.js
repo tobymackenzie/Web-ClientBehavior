ad4d91cdda2a00af435142596592ff23a6aa5d3b
Toby Mackenzie <tobym@tobymackenzie.com>
2013-11-04 17:25:07 -0500

    _incoming: added a few things to be dealt with later

--- _incoming.js
+++ _incoming.js
@@ -1,3 +1,6 @@
+idego: widget, form, button
+ronwhite: ImageFlow
+
 
 /*-------------
 ©objectMonitor
@@ -54,3 +57,104 @@ __.classes.objectMonitor = function(args){
 		}
 	}
 
+/*------
+hovershow
+uses jquery
+-----*/
+__.classes.hoverShow = function(arguments){
+		this.elements = arguments.elements; if(!this.elements) return false;
+		this.selectorShowElement = arguments.selectorShowElement || "show";
+		this.duration = arguments.duration || 500;
+		this.closeTime = arguments.closeTime || 1000;
+		this.classClosed = arguments.classClosed || "closed";
+		this.classOpen = arguments.classOpen || "open";
+
+		this.timeout = false;
+		this.elmCurrent = false;
+		var fncThis = this;
+
+		this.hide(this.elements, 0);
+
+		this.elements.hover(function(){
+				fncThis.show($(this));
+			}
+			, function(){
+				var elmThis = $(this);
+
+				clearTimeout(fncThis.timeout);
+
+				fncThis.timeout = setTimeout(function(){
+					fncThis.hide(elmThis);
+					fncThis.timeout = false;
+					fncThis.elmCurrent = false;
+				}, fncThis.closeTime);
+			});
+
+		this.elements.focusin(function(){fncThis.show($(this));});
+		this.elements.focusout(function(){fncThis.hide($(this));});
+	}
+	__.classes.hoverShow.prototype.show = function(argElement, argDuration){
+		if(!argElement) return false;
+		if(argElement[0] == this.elmCurrent[0]){
+			clearTimeout(this.timeout);
+			return false;
+		}
+		if(!argDuration) argDuration = this.duration;
+		this.hide(this.elements.filter("."+this.classOpen), argDuration);
+		argElement/* .find(this.selectorShowElement) */.animate({top: 0}, {duration: argDuration}).addClass(this.classOpen).removeClass(this.classClosed);
+		this.elmCurrent = argElement;
+	}
+	__.classes.hoverShow.prototype.hide = function(argElement, argDuration){
+		if(!argElement || argElement.length == 0) return false;
+		if(argDuration === undefined) argDuration = this.duration;
+		var offset = argElement.find(this.selectorShowElement).height();
+
+		argElement/* .find(this.selectorShowElement) */.animate({top: -offset+"px"}, {duration: argDuration}).addClass(this.classClosed).removeClass(this.classOpen);
+	}
+
+
+/*=====
+==javascript loaders
+=====*/
+//-@ http://api.jquery.com/jQuery.getScript/
+__.lib.getCachedScript = function(__url, __options){
+	var options = jQuery.extend(__options || {}, {
+		cache: true
+		,dataType: 'script'
+		,url: __url
+	});
+	return jQuery.ajax(options);
+}
+//-@ http://stackoverflow.com/questions/1130921/is-the-callback-on-jquerys-getscript-unreliable-or-am-i-doing-something-wrong
+/*
+				__.lib.loadJS(
+					'/scripts/klass.min.js'
+					,function(){
+						return (typeof klass == 'function');
+					}
+				).done(function(){
+					__.lib.loadJS(
+						'/scripts/code.photoswipe.jquery-3.0.5.js'
+						,function(){
+							return (typeof __.$portfolioListThumbnails.photoSwipe == 'function');
+						}
+					).done(function(){
+						__.$portfolioListThumbnails.photoSwipe();
+					});
+				});
+*/
+__.lib.loadJS = function(__url, __loadTest, __options){
+	var _loadTest = (typeof __loadTest == 'function') ? __loadTest : function(){ return true; };
+	var _options = jQuery.extend(__options || {}, {
+		interval: 200
+	});
+	var $script = jQuery('<script>').attr('src', __url).appendTo('head');
+	var _deferred = new jQuery.Deferred();
+	var _interval = setInterval(function(){
+		if(_loadTest()){
+			clearInterval(_interval);
+			_deferred.resolve('loaded');
+		}
+	}, _options.interval);
+	return _deferred.promise();
+}
