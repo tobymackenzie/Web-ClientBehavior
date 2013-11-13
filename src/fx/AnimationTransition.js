d1ea6e4c2d5c2261f4f2dc7db69166b773e6e990
akronartmuseum.org <aam@thurston.cogneato.com>
2013-11-13 10:28:24 -0500

    AnimationTransition:onBefore(): don't try to do anything with unset elements

--- src/fx/AnimationTransition.js
+++ src/fx/AnimationTransition.js
@@ -162,7 +162,7 @@ define(['jquery', 'tmclasses/tmclasses', 'tmlib/fx/AnimationQueue', 'tmlib/core/
 				var _elms = _args.elements;
 				var _styles;
 				for(var _iElms in _elms){
-					if(_elms.hasOwnProperty(_iElms)){
+					if(_elms.hasOwnProperty(_iElms) && _elms[_iElms]){
 						_styles = (this.stylesBefore && this.stylesBefore[_iElms]) ? this.stylesBefore[_iElms] : null;
 						if(_styles){
 							if(typeof _styles === 'function'){
