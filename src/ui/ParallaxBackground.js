611b6640fe2199d241f264f5ad818d6072ce8e71
Toby Mackenzie <public@tobymackenzie.com>
2015-02-04 22:50:57 -0500

    ui/parallaxbg: disable for IE9-, seemingly was broken

--- src/ui/ParallaxBackground.js
+++ src/ui/ParallaxBackground.js
@@ -3,8 +3,20 @@ define(['jquery', 'tmclasses/tmclasses'], function(jQuery, tmclasses){
 	var __ParallaxBackground = tmclasses.create({
 		init: function(){
 			this.__parent(arguments);
+			//-# dirty hack for now: bail on IE9 minus for centered start, because jQuery doesn't seem to be able to get at a usable value for their `background-position`
+			if(
+				this.calcType === 'centeredStart'
+				//-@ http://www.pinlady.net/PluginDetect/IE/
+				&& new Function("return/*@cc_on!@*/!1")()
+				//-@ http://stackoverflow.com/questions/15657614/identify-ie9-ie10-using-jquery
+				&& !window.requestAnimationFrame
+			){
+				return undefined;
+			}
 			if(this.elm){
-				this.constructor.window.on('load resize scroll', jQuery.proxy(this._changeHandler, this));
+				var _callback = jQuery.proxy(this._changeHandler, this);
+				this.constructor.window.on('load resize scroll', _callback);
+				jQuery(_callback);
 			}
 		}
 		,properties: {
@@ -44,7 +56,13 @@ define(['jquery', 'tmclasses/tmclasses'], function(jQuery, tmclasses){
 							}
 							if(_startingPosition.indexOf('%') !== -1){
 								_startingPosition = parseInt(_startingPosition,10);
+							}else if(_startingPosition.indexOf('px') !== -1){
+								//-# dirty hack for now: just disable everything if pixels, not sure how to handle that.  happens IE8 regardless of what is specified in CSS
+								this.constructor.window.off('resize scroll', jQuery.proxy(this._changeHandler, this));
+								this._changeHandler = function(){};
+								return false;
 							}
+
 							this._startOffset = -1 * (50 + (_bgPosition - 50) * this.movementRatio - _startingPosition);
 						}
 						_bgPosition = 50 + (_bgPosition - 50) * this.movementRatio + this._startOffset;
