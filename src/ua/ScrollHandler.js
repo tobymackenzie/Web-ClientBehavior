566cbe490324f67ed7981625449d8b7e3889b411
Toby Mackenzie <public@tobymackenzie.com>
2015-02-12 16:38:40 -0500

    ua/scrollhandler: throttle publishing of scroll instead of debouncing

--- src/ua/ScrollHandler.js
+++ src/ua/ScrollHandler.js
@@ -13,24 +13,36 @@ define(['jquery', 'tmclasses/tmclasses'], function(jQuery, __tmclasses){
 			this.window.on('scroll', jQuery.proxy(this.handleChange, this));
 		}
 		,properties: {
-			deinit: function(){
+			debounceTimeout: undefined
+			,deinit: function(){
 				this.window.off('scroll', jQuery.proxy(this.handleChange, this));
 			}
 			,delay: 200
 			,handleChange: function(){
 				var _this = this;
-				clearTimeout(this.shortTimeout);
-				this.shortTimeout = setTimeout(function(){
+
+				//--throttle publishing of scroll
+				var _now = new Date();
+				if(_this.throttleLast && _now < _this.throttleLast){
+					clearTimeout(_this.throttleTimeout);
+					_this.throttleTimeout = setTimeout(function(){
+						_this.throttleLast = _now;
+						_this.pub('scroll');
+					});
+				}else{
+					_this.throttleLast = _now;
 					_this.pub('scroll');
-				}, this.shortDelay);
-				clearTimeout(this.timeout);
-				this.timeout = setTimeout(function(){
+				}
+
+				//--debounce publishing of change
+				clearTimeout(_this.debounceTimeout);
+				_this.debounceTimeout = setTimeout(function(){
 					_this.pub('change');
-				}, this.delay);
+				}, _this.delay);
 			}
-			,shortDelay: 40
-			,shortTimeout: undefined
-			,timeout: undefined
+			,throttleLast: undefined
+			,throttleDelay: 40
+			,throttleTimeout: undefined
 			,window: undefined
 		}
 	});
